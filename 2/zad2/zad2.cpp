0d19b2073b2a836eda960e7b123ec92335d20ff7
MarcinBieganek <marcin.bieganek@gmail.com>
2022-11-08 16:36:29 +0100

    Second list shared_ptr task solution

---
+++ 2/zad2/zad2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <memory>
+#include <fstream>
+
+using namespace std;
+
+class Line_writer {
+  	public:
+    	ofstream file;
+		Line_writer() {
+			file.open("file.txt", ios::out);
+		}
+    	virtual ~Line_writer() {
+      		file.close();
+    	}
+};
+
+int main() {
+	auto lw = new Line_writer();
+
+	shared_ptr<Line_writer> alw(lw);
+	shared_ptr<Line_writer> blw(alw);
+	shared_ptr<Line_writer> clw(blw);
+
+	alw->file << "ALW !" << endl;
+	blw->file << "BLW !" << endl;
+	clw->file << "CLW !" << endl;
+
+
+  	return 0;
+}
