59357d04ca6b2695ac224492c01ced252f71129d
MarcinBieganek <marcin.bieganek@gmail.com>
2023-01-24 16:02:41 +0100

    List 10 task 4 small fix with input.

--- 10/zad4/zad4.cpp
+++ 10/zad4/zad4.cpp
@@ -14,45 +14,44 @@ void print_entries(std::vector<fs::directory_entry> v) {
 }
 
 
-int main(int argc, char** argv) {
+int main() {
+    std::string name = "";
+    getline(std::cin, name);
 
+    const fs::path p(name);
 
-    for (int i = 1; i < argc; i++) {
-        const fs::path p(argv[i]);
+    bool ex = fs::exists(p);
+    fs::file_status s = fs::status(p);
 
-        bool ex = fs::exists(p);
-        fs::file_status s = fs::status(p);
+    if (ex) {
+        if (fs::is_directory(s)) {
+            std::cout << "File: \"" << name << "\" is directory" << std::endl;
+            std::cout << "  - canonical path: " << fs::canonical(p) << std::endl;
+            std::cout << "  - content: " << std::endl;
 
-        if (ex) {
-            if (fs::is_directory(s)) {
-                std::cout << "File: \"" << argv[i] << "\" is directory" << std::endl;
-                std::cout << "  - canonical path: " << fs::canonical(p) << std::endl;
-                std::cout << "  - content: " << std::endl;
+            std::vector<fs::directory_entry> dirs;
+            std::vector<fs::directory_entry> files;
 
-                std::vector<fs::directory_entry> dirs;
-                std::vector<fs::directory_entry> files;
+            for(const fs::directory_entry& entry : fs::directory_iterator(p)) {
+                fs::file_status entry_status = fs::status(entry);
 
-                for(const fs::directory_entry& entry : fs::directory_iterator(p)) {
-                    fs::file_status entry_status = fs::status(entry);
-
-                    if (fs::is_directory(entry_status)) {
-                        dirs.push_back(entry);
-                    } else if (fs::is_regular_file(entry_status)) {
-                        files.push_back(entry);
-                    }
+                if (fs::is_directory(entry_status)) {
+                    dirs.push_back(entry);
+                } else if (fs::is_regular_file(entry_status)) {
+                    files.push_back(entry);
                 }
-                
-                std::sort(dirs.begin(), dirs.end());
-                std::sort(files.begin(), files.end());
-
-                print_entries(dirs);
-                print_entries(files);
-            } else {
-                std::cerr << "File: \"" << argv[i] << "\" is not a directory." << std::endl;
             }
+            
+            std::sort(dirs.begin(), dirs.end());
+            std::sort(files.begin(), files.end());
+
+            print_entries(dirs);
+            print_entries(files);
         } else {
-            std::cerr << "File: \"" << argv[i] << "\" does not exists." << std::endl;
+            std::cerr << "File: \"" << name << "\" is not a directory." << std::endl;
         }
+    } else {
+        std::cerr << "File: \"" << name << "\" does not exists." << std::endl;
     }
 
   	return 0;
